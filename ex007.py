75e4f56ca006a911abb9fc8b2887b86691049a9d
Jojowam <villareal.sec@gmail.com>
2023-10-27 16:05:26 -0300

    Adicionando o Exercício 07 na Branch: exercises

---
+++ ex007.py
@@ -0,0 +1,6 @@
+firtNote = int(input('Digite a primeira nota: '))
+secondNote = int(input('Digite a segunda nota: '))
+
+average = (firtNote + secondNote) / 2
+
+print('A média entre {} e {} é igual a {}!' .format(firtNote, secondNote, average))

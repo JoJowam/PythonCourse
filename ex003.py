9e8fa234ce5637575d741e48013c1f3804c1c26b
Jojowam <villareal.sec@gmail.com>
2023-10-27 14:53:23 -0300

    Foram feitos os exercícios de 01 a 06 do curso de Python em vídeo.

---
+++ ex003.py
@@ -0,0 +1,6 @@
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+
+sum = number1 + number2
+
+print('A soma entre {} e {} é igual a {}!' .format(number1, number2, sum))
---
+++ ex005.py
@@ -0,0 +1,2 @@
+number = int(input('Digite um número: '))
+print("O Antecessor de {} é {} e o Sucessor é {}!" .format(number, (number - 1), (number + 1)))
